43e02386501f3d56e3840d7c91dba9d524e6653e
Lucas Pintos <lucaspintos909@gmail.com>
2020-12-28 19:50:05 -0300

    Array de productos, para incluir luego en bd

--- django_store/views.py
+++ django_store/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html', {
-        #Contexto
-    })
+
+    products=[ # ESTO ES PARA PRUEBAS, QUITAR AL INCLUIR BD
+            {'title': 'Camiseta', 'price' : 750, 'stock': True},
+            {'title': 'Buzo', 'price' : 1500, 'stock': True},
+            {'title': 'Crocks', 'price' : 1200, 'stock': False},
+            {'title': 'Campera', 'price' : 1340, 'stock': True},
+            {'title': 'Gorro de lana', 'price' : 800, 'stock': False}
+    ]
+
+    context= {
+        'title': 'Django Store',
+        'text_button': 'Prueba',
+        'products': products
+        
+    }
+
+    return render(request, 'index.html', context)
