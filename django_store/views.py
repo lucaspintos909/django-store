0486d1131fcddfa945f25b3db637f1470aa0773a
Lucas Pintos <lucaspintos909@gmail.com>
2021-01-08 11:46:45 -0300

    Restringiendo accesso a login y a registro si esta logueado.

--- django_store/views.py
+++ django_store/views.py
@@ -31,6 +31,9 @@ def index(request):
     return render(request, 'index.html', context)
 
 def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -51,6 +54,9 @@ def logout_view(request):
     return redirect('login')
 
 def register(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+
     form = RegisterForm( request.POST or None )
 
     if request.method == "POST" and form.is_valid():
