80d4c1045edb31c5b66175d50a21fbdb2c0c860d
Lucas Pintos <lucaspintos909@gmail.com>
2021-01-17 13:46:50 -0300

    Moviendo los productos al archivo views de Product

--- django_store/urls.py
+++ django_store/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+from products.views import ProductListView
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', ProductListView.as_view(), name='index'),
     path('usuarios/login', views.login_view, name='login'),
     path('usuarios/logout', views.logout_view, name='logout'),
     path('usuarios/registro', views.register, name='register'),
--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Product
 
-# Create your views here.
+class ProductListView(ListView):
+    template_name = 'index.html'
+    queryset = Product.objects.all().order_by('-id')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
