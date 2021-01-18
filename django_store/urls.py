ccbc8c53579df8baf3bc2381d5ad6d830023a429
Lucas Pintos <lucaspintos909@gmail.com>
2021-01-18 12:44:24 -0300

    Urls para productos

--- django_store/urls.py
+++ django_store/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from django.urls import include
 
 from products.views import ProductListView
+from . import views
+
+
 urlpatterns = [
     path('', ProductListView.as_view(), name='index'),
     path('usuarios/login', views.login_view, name='login'),
     path('usuarios/logout', views.logout_view, name='logout'),
     path('usuarios/registro', views.register, name='register'),
+    path('productos/', include('products.urls')),
     path('admin/', admin.site.urls),
 ]
