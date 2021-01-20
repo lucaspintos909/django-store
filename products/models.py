6a42865c462619f71fa7d20f7dd94eeb533327b6
Lucas Pintos <lucaspintos909@gmail.com>
2021-01-19 21:33:12 -0300

    Añadiendo validacion para que el slug no se repita, agregando caracteres aleatorios.

--- products/models.py
+++ products/models.py
@@ -1,6 +1,9 @@
+import uuid
 from django.db import models
 from django.utils.text import slugify
 from django.db.models.signals import pre_save
+
+
 
 class Product(models.Model):
     title = models.CharField(max_length = 40)
@@ -13,6 +16,17 @@ class Product(models.Model):
         return self.title
 
 def set_slug(sender, instance, *args, **kwargs):
+
+    if instance.title and not instance.slug:
+
+        slug = slugify(instance.title)
+        
+        while Product.objects.filter(slug=slug).exists():
+            slug = slugify(
+                '{}-{}'.format(instance.title, str(uuid.uuid4())[:8] )
+            )
+
+
     instance.slug = slugify(instance.title)
 
 pre_save.connect(set_slug, sender = Product)
