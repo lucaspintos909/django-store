671cdb6f9417a5254421e43412bb4c797fa60557
Lucas Pintos <lucaspintos909@gmail.com>
2021-01-19 21:23:04 -0300

    Añadiendo slug a la url en vez de poner el id

--- products/models.py
+++ products/models.py
@@ -1,12 +1,18 @@
 from django.db import models
-
-# Create your models here.
+from django.utils.text import slugify
+from django.db.models.signals import pre_save
 
 class Product(models.Model):
-    title = models.CharField(max_length = 20)
+    title = models.CharField(max_length = 40)
     description = models.TextField()
-    price = models.DecimalField(max_digits=8, decimal_places=2, default=0.0)
+    price = models.DecimalField(max_digits = 8, decimal_places = 2, default = 0.0)
+    slug = models.SlugField(null = False, blank = False, unique = True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+def set_slug(sender, instance, *args, **kwargs):
+    instance.slug = slugify(instance.title)
+
+pre_save.connect(set_slug, sender = Product)
--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<pk>', views.ProductDetailView.as_view(), name = 'product') # Primary key
+    path('<slug:slug>', views.ProductDetailView.as_view(), name = 'product')
 ]
