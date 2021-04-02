c56fbec0538ebe541d5c1b707674c1a57079e9f7
Lucas Pintos <lucaspintos909@gmail.com>
2021-04-02 11:53:25 -0300

    [FEAT] Añadiendo responsive al slider

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,10 @@
 window.onload = function() {
     var swiper = new Swiper('.swiper-container', {
-        slidesPerView: 3,
-        spaceBetween: 30,
+        slidesPerView: 4,
+        spaceBetween: 40,
         slidesPerGroup: 3,
         loop: true,
-        loopFillGroupWithBlank: true,
+        loopFillGroupWithBlank: false,
         pagination: {
         el: '.swiper-pagination',
         clickable: true,
@@ -13,6 +13,25 @@ window.onload = function() {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
         },
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+              slidesPerGroup: 2,
+            },
+            // when window width is >= 480px
+            480: {
+              slidesPerView: 2,
+              spaceBetween: 30,
+              slidesPerGroup: 2,
+            },
+            // when window width is >= 640px
+            640: {
+              slidesPerView: 4,
+              spaceBetween: 40
+            }
+        }
     });
     
   };
